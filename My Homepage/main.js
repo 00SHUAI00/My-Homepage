c48e26f3c2a88fa6279cbb0de30ee1e6a94b1d27
shuai <s.wang.pj@gmail.com>
2016-06-07 17:39:45 -0700

    1st commit

---
+++ My Homepage/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  applyNavigation();
+});
+
+function applyNavigation()
+{
+	applyClickEvent();
+	applyNavigationFixForPhone();
+}
+
+function applyClickEvent()
+{
+	$('a[href*=#]').on('click', function(e)
+	{
+		e.preventDefault();
+		
+		if( $( $.attr(this, 'href') ).length > 0 )
+		{
+			$('html, body').animate(
+			{
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 400);
+		}
+		return false;
+	});
+}
+
+function applyNavigationFixForPhone()
+{
+	$('.navbar li a').click(function(event) 
+	{
+		$('.navbar-collapse').removeClass('in').addClass('collapse');
+	});
+}
+
+
+
